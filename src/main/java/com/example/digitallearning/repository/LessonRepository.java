5b20769b2b6ce5c3af73224a5a19956770656be7
kartikeysharma <kartiksharma768976@gmail.com>
2026-03-25 13:47:53 +0530

    Delete src directory

--- src/main/java/com/example/digitallearning/repository/LessonRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.digitallearning.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.digitallearning.entity.Lesson;
-
-public interface LessonRepository extends JpaRepository<Lesson,Long>{}
--- src/main/java/com/example/digitallearning/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.digitallearning.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.digitallearning.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student,Long>{
-    Optional<Student> findByEmail(String email);
-}
